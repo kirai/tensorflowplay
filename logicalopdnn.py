dca30a09daedf1e76d400d382a30581878870544
kirai <hectorgarcia@gmail.com>
2016-07-13 11:25:59 -0400

    XOR

--- logicalopdnn.py
+++ logicalopdnn.py
@@ -1,33 +1,28 @@
+# -*- coding: utf-8 -*-
 """
 Logical operators DNN training
 """
-
-#tlearn logical
-#http://tflearn.org
-
-#https://medium.com/@ilblackdragon
-# https://raw.githubusercontent.com/tflearn/tflearn/master/examples/basics/logical.py
-# http://www.jorditorres.org/introduccion-practica-al-deep-learning-con-tensorflow-de-google-parte-5/
-# http://www.jorditorres.org/libro-hello-world-en-tensorflow/
-# http://www.jorditorres.org/wp-content/uploads/2016/02/FirstContactWithTensorFlow.part1_.pdf
 
 from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
 import tflearn
 
-def train_dnn(X, Y, input_num, first_layer, second_layer):
-    # Graph definition
+# Graph definition
+def define_dnn_topology(input_num, first_layer, second_layer):
     with tf.Graph().as_default():
         g = tflearn.input_data(shape=[None, input_num])
         g = tflearn.fully_connected(g, first_layer, activation='linear')
         g = tflearn.fully_connected(g, second_layer, activation='linear')
         g = tflearn.fully_connected(g, 1, activation='sigmoid')
         g = tflearn.regression(g, optimizer='sgd', learning_rate=2., loss='mean_square')
-	
-        #Model training
-        m = tflearn.DNN(g)
-        m.fit(X, Y, n_epoch=100, snapshot_epoch=False)
+
+    return g 
+
+#Model training
+def train_dnn(X, Y, g, n_epoch):
+    m = tflearn.DNN(g)
+    m.fit(X, Y, n_epoch=n_epoch, snapshot_epoch=False)
 
     return m
 
@@ -35,7 +30,8 @@ def train_dnn(X, Y, input_num, first_layer, second_layer):
 X = [[0.], [1.]]
 Y = [[1.], [0.]]
 
-m = train_dnn(X, Y, 1, 128, 128)
+g = define_dnn_topology(1, 128, 128)
+m = train_dnn(X, Y, g, 100)
 
 # Test model
 print("Testing NOT operator")
@@ -46,7 +42,8 @@ print("NOT 1:", m.predict([[1.]]))
 X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
 Y = [[0.], [1.], [1.], [1.]]
 
-m = train_dnn(X, Y, 2, 128, 128)
+g = define_dnn_topology(2, 128, 128)
+m = train_dnn(X, Y, g, 100)
 
 #Test model
 print("Testing OR operator")
@@ -59,7 +56,8 @@ print("1 or 1:", m.predict([[1., 1.]]))
 X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
 Y = [[0.], [0.], [0.], [1.]]
 
-m = train_dnn(X, Y, 2, 128, 128)
+g = define_dnn_topology(2, 128, 128)
+m = train_dnn(X, Y, g, 100)
 
 print("Testing AND operator")
 print("0 and 0:", m.predict([[0., 0.]]))
@@ -72,7 +70,17 @@ X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
 Y_nand = [[1.], [1.], [1.], [0.]]
 Y_or = [[0.], [1.], [1.], [1.]]
 
-m = train_dnn(
-# m = train_dnn(X, Y, 2, 32, 32)
+g_nand = define_dnn_topology(2, 32, 32) 
+g_or = define_dnn_topology(2, 32, 32)
 
+# XOR Merging Nand and OR operators 
+g_xor = tflearn([g_nand, g_or], mode='elewise_mul')
+m = train_dnn(X, [Y_nand, Y_or], g_xor, 400)
+
+# Testing
+print("Testing XOR operator")
+print("0 xor 0:", m.predict([[0., 0.]]))
+print("0 xor 1:", m.predict([[0., 1.]]))
+print("1 xor 0:", m.predict([[1., 0.]]))
+print("1 xor 1:", m.predict([[1., 1.]]))
 
