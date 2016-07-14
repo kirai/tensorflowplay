af4ece0eb3f1296cc78b5c2c83d87c3ebca60296
kirai <hectorgarcia@gmail.com>
2016-07-14 16:08:04 -0400

    Seiri

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Personal playground for Deep Learning
+
+
+MNIST
+=====
+* mnist_train_save.py
+Trains and saves a model
+
+* mnist_eval_loadfrom_hdf5.py
+Evaluates a previously trained model agains MNIST 10000 test samples
+
--- mnist/mnist_eval_loadfrom_hdf5.py
+++ mnist/mnist_eval_loadfrom_hdf5.py
@@ -9,23 +9,19 @@ from keras.models import Sequential
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
 X_test = X_test.astype('float32')
 X_test /= 255
-
 nb_classes = 10
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 # Load trained model
 json_string = open('model.json').read()  
 model_config = model_from_json(json_string).get_config()
-
 model = Sequential.from_config(model_config)
 
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adadelta', metrics=['accuracy'])
-
 model.load_weights('modelweights.hdf5')
 
 # Evaluate with previously trained model
-score = model.evaluate(X_test, Y_test, verbose = 1)
+score = model.evaluate(X_test, Y_test, verbose = 0)
 
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
-
