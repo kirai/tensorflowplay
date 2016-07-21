9b559c91c5f694cc3bc4be46dc35ea61be7c6e99
kirai <hectorgarcia@gmail.com>
2016-07-21 14:28:51 +0900

    Output model to csv file

--- nlpplay/bagofwords.py
+++ nlpplay/bagofwords.py
@@ -30,4 +30,38 @@ if __name__ == '__main__':
     for i in range( 0, len(train["review"])):
         clean_train_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train["review"][i], True)))
 
+    print("Creating the bag of words...\n")
+
+    vectorizer = CountVectorizer(analyzer = "word",   \
+                                 tokenizer = None,    \
+                                 preprocessor = None, \
+                                 stop_words = None,   \
+                                 max_features = 5000)
+
+    train_data_features = vectorizer.fit_transform(clean_train_reviews)
+    train_data_features = train_data_features.toarray()
+    
+    # Random forest classifier with 100 trees
+    forest = RandomForestClassifier(n_estimators = 100)
+    forest = forest.fit( train_data_features, train["sentiment"])
+
+    clean_test_reviews = []
+
+    print("Cleaning and parsing the test set movie reviews...\n")
+    for i in range(0,len(test["review"])):
+        clean_test_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], True)))
+
+    # Get a bag of words for the test set, and convert to a numpy array
+    test_data_features = vectorizer.transform(clean_test_reviews)
+    test_data_features = test_data_features.toarray()
+
+    # Use the random forest to make sentiment label predictions
+    print("Predicting test labels...\n")
+    result = forest.predict(test_data_features)
+        
+    # Copy the results to a pandas dataframe with an "id" column and a "sentiment" column
+    output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
+
+    output.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'Bag_of_Words_model.csv'), index=False, quoting=3)
+    print("Wrote results to Bag_of_Words_model.csv")
 
